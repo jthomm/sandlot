a6668ea3b8a3ef32ba0be9fe19cf9ae06cc9dc99
jthomm <jthomm@yahoo.com>
2014-04-22 00:04:40 -0400

    Add sql to delete records pertaining to a single game from all tables

---
+++ sandlot/db/delete_game.sql
@@ -0,0 +1,50 @@
+  DELETE
+    FROM pitch
+   WHERE EXISTS (SELECT NULL
+                   FROM at_bat
+                  WHERE at_bat.game_id = ?
+                    AND at_bat.at_bat_id = pitch.at_bat_id)
+;
+
+  DELETE
+    FROM runner
+   WHERE EXISTS (SELECT NULL
+                   FROM at_bat
+                  WHERE at_bat.game_id = ?
+                    AND at_bat.at_bat_id = runner.at_bat_id)
+;
+
+  DELETE
+    FROM starter
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM action
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM at_bat
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM game_player
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM game_coach
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM game_umpire
+   WHERE game_id = ?
+;
+
+  DELETE
+    FROM game
+   WHERE game_id = ?
+;
