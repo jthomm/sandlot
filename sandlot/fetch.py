c43584e94f5715ce7694970a6efa315828787732
jthomm <jthomm@yahoo.com>
2015-03-29 15:37:01 -0400

    Add some more games, I guess

--- sandlot/fetch.py
+++ sandlot/fetch.py
@@ -5,17 +5,17 @@ from time import sleep
 
 
 class GameDay(object):
-#
+
     def __init__(self, date):
         self.date = date
-#
+
     @property
     def url(self):
         year = unicode(self.date.year)
         month = unicode(self.date.month).zfill(2)
         day = unicode(self.date.day).zfill(2)
         return u'http://gdx.mlb.com/components/game/mlb/year_{year}/month_{month}/day_{day}/'.format(year=year, month=month, day=day)
-#
+
     @property
     def game_ids(self):
         soup = BS(urlopen(self.url).read())
@@ -25,23 +25,23 @@ class GameDay(object):
 
 
 class GameResource(object):
-#
+
     def __init__(self, game_id, game_day):
         self.game_id = game_id
         self.game_day = game_day
-#
+
     @property
     def base_url(self):
         return self.game_day.url + self.game_id
-#
+
     @property
     def inning_all_url(self):
         return self.base_url + '/inning/inning_all.xml'
-#
+
     @property
     def inning_hit_url(self):
         return self.base_url + '/inning/inning_hit.xml'
-#
+
     @property
     def players_url(self):
         return self.base_url + '/players.xml'
@@ -49,14 +49,14 @@ class GameResource(object):
 
 
 class XMLResource(object):
-#
+
     def __init__(self, url):
         self.url = url
-#
+
     @property
     def response_string(self):
         return urlopen(self.url).read()
-#
+
     def save(self, file_name):
         response_string = self.response_string
         with gzip.open(file_name, 'wb') as f:
@@ -64,33 +64,40 @@ class XMLResource(object):
 
 
 
-#from os import listdir
-#for date in sorted(list(set(f[4:14] for f in listdir('./xml/2014/')))): date
-
+from os import popen
 import datetime
 
 one_day = datetime.timedelta(days=1)
 
-start = datetime.date(2014, 7, 23)
-until = datetime.date(2014, 7, 24)
+latest = popen("ls xml/* | awk -F'_' '{ print $2$3$4 }' | tail -1").read().strip()
+
+start = datetime.datetime.strptime(latest, '%Y%m%d').date() + one_day
+until = datetime.date.today() - datetime.timedelta(hours=26)
+
+#start = datetime.date(2014, 7, 23)
+#until = datetime.date(2014, 7, 24)
 
 while start < until:
     print start
     print '=================================================='
     game_day = GameDay(start)
+    data_dir = './xml/{0}'.format(start.year)
     for game_id in game_day.game_ids:
         print 'Fetching data for %s' % game_id
         gr = GameResource(game_id, game_day)
         inning_all = XMLResource(gr.inning_all_url)
         try:
-            inning_all.save('./xml/2014/%s-inning_all.xml.gz' % game_id)
+            inning_all.save('{0}/{1}-inning_all.xml.gz'.format(data_dir, game_id))
+            #inning_all.save('./xml/2014/%s-inning_all.xml.gz' % game_id)
         except HTTPError:
             print 'WARNING:  No data for %s - skipping...\n' % game_id
             continue
         inning_hit = XMLResource(gr.inning_hit_url)
-        inning_hit.save('./xml/2014/%s-inning_hit.xml.gz' % game_id)
+        inning_hit.save('{0}/{1}-inning_hit.xml.gz'.format(data_dir, game_id))
+        #inning_hit.save('./xml/2014/%s-inning_hit.xml.gz' % game_id)
         players = XMLResource(gr.players_url)
-        players.save('./xml/2014/%s-players.xml.gz' % game_id)
+        players.save('{0}/{1}-players.xml.gz'.format(data_dir, game_id))
+        #players.save('./xml/2014/%s-players.xml.gz' % game_id)
         print 'Got it!\n'
     start += one_day
     print 'Sleeping...\n'
